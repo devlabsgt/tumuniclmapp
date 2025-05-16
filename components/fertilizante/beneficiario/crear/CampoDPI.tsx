bf3e654e52d447007019a9857a83f4a61e693fcc
Óscar Jiménez <devlabsgt@gmail.com>
2025-05-16 11:48:01 -0600

    Se mejoro el ingreso de numeros2

--- components/fertilizante/beneficiario/crear/CampoDPI.tsx
+++ components/fertilizante/beneficiario/crear/CampoDPI.tsx
@@ -8,13 +8,18 @@ interface CampoDPIProps {
 }
 
 export default function CampoDPI({ dpi, setDpi, verificarDPI }: CampoDPIProps) {
+  const handleDpiChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const soloNumeros = e.target.value.replace(/\D/g, '').slice(0, 13);
+    setDpi(soloNumeros);
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <Input
         type="text"
         placeholder="Ingrese el DPI del beneficiario"
         value={dpi}
-        onChange={(e) => setDpi(e.target.value)}
+        onChange={handleDpiChange}
       />
       <Button onClick={verificarDPI} className="h-11 text-lg">
         Ingresar DPI
