a9b8271dec573209a71282bdc16598f5606b10c8
Óscar Jiménez <devlabsgt@gmail.com>
2025-09-10 18:15:25 -0600

    Creacion del modulo de comisiones, municipalidad de clm7

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 
 import type { NextConfig } from "next";
 import withPWAInit from "next-pwa";
-import path from "path"; // Se añade la importación de 'path'
 
 // 1. Definir la configuración de la PWA
 const withPWA = withPWAInit({
@@ -15,16 +14,6 @@ const withPWA = withPWAInit({
 // 2. Su configuración existente de Next.js
 const nextConfig: NextConfig = {
   /* config options here */
-  
-  // --- SE AÑADE ESTA SECCIÓN PARA RESOLVER EL ALIAS ---
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname),
-    };
-    return config;
-  },
-  // ---------------------------------------------------
 };
 
 // 3. Envolver su configuración con la de PWA y exportar
