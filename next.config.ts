8e7636930940e00ffc08602139394694fd0a7e84
Óscar Jiménez <devlabsgt@gmail.com>
2025-09-10 18:19:33 -0600

    Creacion del modulo de comisiones, municipalidad de clm8

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 // next.config.ts
-
 import type { NextConfig } from "next";
 import withPWAInit from "next-pwa";
 
-// 1. Definir la configuración de la PWA
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
@@ -11,10 +9,8 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === "development",
 });
 
-// 2. Su configuración existente de Next.js
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-// 3. Envolver su configuración con la de PWA y exportar
 export default withPWA(nextConfig);
